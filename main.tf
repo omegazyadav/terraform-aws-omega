406c2cac8b996013e26120531ef8ffb8d547cac3
Yadav Lamichhane <omegazyadav1@gmail.com>
2022-04-19 18:10:28 +0545

    fix: input variable file

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 module "website_s3_bucket" {
   source = "./modules/aws-s3-bucket"
 
-  bucket_name = "omegazyadav-2020-12-22"
+  bucket_name = var.bucket_name
 
   tags = {
     Terraform   = "true"
